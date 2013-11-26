54edc78ee3733e244162c2471398ae9c882d5c3b
moba <m@b>
2013-11-26 12:28:50 +0100

    updated some settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,11 +9,16 @@ SITEURL = ''
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
+DEFAULT_CATEGORY = 'torservers'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+
+FEED_ALL_RSS = 'feed/all.rss.xml'
+CATEGORY_FEED_RSS = 'feed/%s.rss.xml'
 TRANSLATION_FEED_ATOM = None
+
+ARTICLE_URL = '{date:%Y}{date:%m}{date:%d}/{slug}.html'
+ARTICLE_SAVE_AS = ARTICLE_URL
 
 # Blogroll
 LINKS =  (('Torproject', 'https://torproject.org/'),
@@ -28,6 +33,7 @@ DEFAULT_PAGINATION = False
 ARTICLE_EXCLUDES = ('theme', 'output',)
 THEME = "theme/tuxlite_tbs"
 
+TYPOGRIFY = True
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
